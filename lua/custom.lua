ff978983c30ff0791235ed19c5d3f8a42d867541
ACertainPersons <127463096+ACertainPersons@users.noreply.github.com>
2024-01-21 20:33:12 -0600

    Fixed autoclose

--- lua/custom.lua
+++ lua/custom.lua
@@ -46,6 +46,31 @@ require("catppuccin").setup({
     },
 })
 
+require("autoclose").setup({
+    keys = {
+      ["("] = { escape = false, close = true, pair = "()" },
+      ["["] = { escape = false, close = true, pair = "[]" },
+      ["{"] = { escape = false, close = true, pair = "{}" },
+
+      [">"] = { escape = true, close = false, pair = "<>" },
+      [")"] = { escape = true, close = false, pair = "()" },
+      ["]"] = { escape = true, close = false, pair = "[]" },
+      ["}"] = { escape = true, close = false, pair = "{}" },
+
+      ['"'] = { escape = true, close = true, pair = '""' },
+      ["'"] = { escape = true, close = true, pair = "''" },
+      ["`"] = { escape = true, close = true, pair = "``" },
+   },
+   options = {
+      disabled_filetypes = { "text" },
+      disable_when_touch = true,
+      touch_regex = "[%w(%[{]",
+      pair_spaces = false,
+      auto_indent = true,
+      disable_command_mode = false,
+   },
+})
+
 -- setup must be called before loading
 vim.cmd.colorscheme "catppuccin"
 
